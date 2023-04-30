83a554e80f85ae86eb25e0e6b64f72ec33d51a08
4accccc <78542774+4accccc@users.noreply.github.com>
2023-04-30 22:39:31 +0800

    3
    3

--- native/src/include/magisk.hpp
+++ native/src/include/magisk.hpp
@@ -29,7 +29,7 @@ extern std::string    MAGISKTMP;
 #define ZYGISKBIN     INTLROOT "/zygisk"
 #define SELINUXMOCK   INTLROOT "/selinux"
 
-constexpr const char *applet_names[] = { "su", "resetprop", nullptr };
+constexpr const char *applet_names[] = { "suu", "resetprop", nullptr };
 
 #define POST_FS_DATA_WAIT_TIME       40
 #define POST_FS_DATA_SCRIPT_MAX_TIME 35
